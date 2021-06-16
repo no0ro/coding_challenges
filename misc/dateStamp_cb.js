32b27cabf4261aaabffd5f89b382eae6b6338a98
no0ro <nrolwes@gmail.com>
2021-06-16 10:16:03 -0400

    date stamp working ssolution and test logs

--- misc/dateStamp_cb.js
+++ misc/dateStamp_cb.js
@@ -4,3 +4,17 @@
 // represented as a human-readable string (see the Date object for conversion methods), and an output 
 // key that contains the result from invoking the passed-in function.
 
+function dateStamp(cb) {
+    return function(...args) {
+      let obj = {}
+      let currentDate = new Date
+      
+      obj["date"] = currentDate.toDateString()
+      obj["output"] = cb(...args)
+      return obj
+    }
+  }
+  
+  const stampedMultBy2 = dateStamp(n => n * 2);
+  console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+  console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
