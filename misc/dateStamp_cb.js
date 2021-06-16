761780438066a5d51cdf17b7cebff77ff0d5eabb
no0ro <nrolwes@gmail.com>
2021-06-16 10:15:20 -0400

    date stamp prompt

---
+++ misc/dateStamp_cb.js
@@ -0,0 +1,6 @@
+// Create a function dateStamp that accepts a function and returns a function. 
+// The returned function will accept whatever arguments the passed-in function accepts 
+// and return an object with a date key whose value is today's date (not including the time)
+// represented as a human-readable string (see the Date object for conversion methods), and an output 
+// key that contains the result from invoking the passed-in function.
+
