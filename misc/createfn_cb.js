4bb8624c833b78dc790c511c02613de4d7abb67e
no0ro <nrolwes@gmail.com>
2021-06-22 17:35:58 -0400

    added solution and wording breakdown

--- misc/createfn_cb.js
+++ misc/createfn_cb.js
@@ -1,8 +1,8 @@
 // PROMPT: 
 // Create a function createFunction that creates and returns a function. 
 // When that newly created function is called, it should return the string 'hello world'.
-//          const myFunction = createFunction();
-//          console.log(myFunction()); //should log: 'hello world'
+//   const myFunction = createFunction();
+//   console.log(myFunction()); //should log: 'hello world'
 
 // PSEUDO: 
 // create function createFunction
@@ -11,7 +11,24 @@
     // return newFunction
 
 
+function createFunction() {
 
+    function newFunction() {
+        return 'hello world';
+    }
+
+    return newFunction;
+};
 
 const myFunction = createFunction();
 console.log( myFunction() ); //should log: 'hello world'
+
+
+
+// WORDING:
+// I set up a called variable myFunction.
+// I set it equal to the evaluated result of createFunction()
+// createdFunction() will return the function definition & closure (PLSRD) of newFunction()
+// then, we console.log the invocation of myFunction()
+// we will see the eveluated result of newFunction()....it's just being run under its new name, myFunction
+// 
