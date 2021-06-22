8d622293dce95f39b0cf62d70bf437faa40f490a
no0ro <nrolwes@gmail.com>
2021-06-22 17:11:35 -0400

    added create function callback questions prompt

---
+++ misc/createfn_cb.js
@@ -0,0 +1,11 @@
+// Create a function createFunction that creates and returns a function. 
+// When that newly created function is called, it should return the string 'hello world'.
+//          const myFunction = createFunction();
+//          console.log(myFunction()); //should log: 'hello world'
+
+
+
+
+
+
+// console.log( myFunction() ); //should log: 'hello world'
