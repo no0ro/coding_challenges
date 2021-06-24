a7ff3040c2971883e2a2951fe84c9c68cd94e07e
no0ro <nrolwes@gmail.com>
2021-06-24 13:16:00 -0400

    added test console.log

--- misc/closure_defineFirstArg.js
+++ misc/closure_defineFirstArg.js
@@ -13,6 +13,7 @@ function defineFirstArg(cb, arg){
 
 const subtract = function(big, small) { return big - small; };
 const subFrom20 = defineFirstArg(subtract, 20);  
+console.log(subFrom20(5)); // should log: 15
 
 
 
