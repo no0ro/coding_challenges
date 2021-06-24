08433bda2268ed823163baec43e975d3e7c3f468
no0ro <nrolwes@gmail.com>
2021-06-24 13:08:55 -0400

    updated file name

--- misc/closure_dateStamp.js
+++ misc/closure_dateStamp.js
@@ -18,13 +18,3 @@ function dateStamp(cb) {
   const stampedMultBy2 = dateStamp(n => n * 2);
   console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
   console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
-
-  function createFunction () {
-
-    function newFunction () {
-        return 'hello world'
-    };
-
-    return newFunction
-};
-
