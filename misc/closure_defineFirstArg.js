f3f4a1686c1336e2ac1043cfcd00dc9455b23e87
no0ro <nrolwes@gmail.com>
2021-06-24 13:07:24 -0400

    added prompt

---
+++ misc/closure_defineFirstArg.js
@@ -0,0 +1,4 @@
+// Prompt:  Create a function defineFirstArg that accepts a function and an argument. 
+// Also, the function being passed in will accept at least one argument. 
+// defineFirstArg will return a new function that invokes the passed-in function with the passed-in argument as the passed-in function's first argument. 
+// Additional arguments needed by the passed-in function will need to be passed into the returned function.
