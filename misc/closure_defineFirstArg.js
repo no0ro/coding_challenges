0a70c5df8f9c4cde9dd0447ceecc8eb6b57489b8
no0ro <nrolwes@gmail.com>
2021-06-24 13:15:21 -0400

    added solution and Notes template

--- misc/closure_defineFirstArg.js
+++ misc/closure_defineFirstArg.js
@@ -1,4 +1,34 @@
-// Prompt:  Create a function defineFirstArg that accepts a function and an argument. 
-// Also, the function being passed in will accept at least one argument. 
-// defineFirstArg will return a new function that invokes the passed-in function with the passed-in argument as the passed-in function's first argument. 
-// Additional arguments needed by the passed-in function will need to be passed into the returned function.
+/* Prompt:  
+Create a function defineFirstArg that accepts a function and an argument. 
+Also, the function being passed in will accept at least one argument. 
+defineFirstArg will return a new function that invokes the passed-in function with the passed-in argument as the passed-in function's first argument. 
+Additional arguments needed by the passed-in function will need to be passed into the returned function.
+*/
+
+function defineFirstArg(cb, arg){
+    return function (...args){
+      return cb(arg, ...args)
+    }
+  }
+
+const subtract = function(big, small) { return big - small; };
+const subFrom20 = defineFirstArg(subtract, 20);  
+
+
+
+/* PSUDO:
+
+*/
+
+/* WORDING:
+
+*/
+
+
+
+/* NOTES & TAKE AWAYS:
+
+`...args`
+
+
+*/
