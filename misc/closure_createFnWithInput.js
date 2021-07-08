30d000c6fbf1c0e66963a9b2e4550378127bd494
no0ro <nrolwes@gmail.com>
2021-07-07 23:56:04 -0400

    added file with prompt

---
+++ misc/closure_createFnWithInput.js
@@ -0,0 +1,8 @@
+// Prompt: Create a function createFunctionWithInput that accepts one input and returns a function. When that created function is called, it should return the input that was passed to createFunctionWithInput when it was created.
+// const sampleFunc = createFunctionWithInput('sample');
+// const helloFunc = createFunctionWithInput('hello');
+// // Now we'll call the functions we created and log the result
+// console.log(sampleFunc()); 
+// // should log 'sample' to the console
+// console.log(helloFunc());
+// // should log 'hello' to the console
