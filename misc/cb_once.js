ffdc913c32d2c88a3cdce2e090904812395caf3d
no0ro <nrolwes@gmail.com>
2021-07-08 11:32:44 -0400

    added consolelog confirmation

--- misc/cb_once.js
+++ misc/cb_once.js
@@ -27,3 +27,6 @@ function once(callback){ // function(num)
     return num + 2;
   });
   
+  console.log(addByTwoOnce(5));  //should log 7
+  console.log(addByTwoOnce(10));  //should log 7
+  console.log(addByTwoOnce(9001));  //should log 7
