751414c710ad1b554e5b918e4df79c1d95150afb
no0ro <nrolwes@gmail.com>
2021-07-14 01:12:56 -0400

    cleaned up solution, added improvement ideas, removed comments

--- misc/fizzbuzz/recursive_fizzbuzz.js
+++ misc/fizzbuzz/recursive_fizzbuzz.js
@@ -4,23 +4,49 @@ Print integers 1 to N, but print “Fizz” if an integer is divisible by 3,
 and “FizzBuzz” if an integer is divisible by both 3 and 5
 */
 
-// (i % 3 === 0 && i % 5 === 0)
 
 
-let final = []
-function fizzBuzz(num) {
+// Must print Ascending order! would be easier if i could just `num--`
 
-    // let resultArr = []
-    // let num = n
-    if (num >= 1){
-    //    resultArr.push(num)
-    //    console.log(num)
-    //    let updatedResult = resultArr
-        final.push(num)
-       return fizzBuzz( num - 1)
+// IMPROVEMENTS: 
+//      1. remove global array variable (place inside param?) 
+//      2. remove `1` from initial `fizzBuzz(1, 16)` call. issue i think: bc `counter = 1` is optional, so probs cant return an optional arg before its actually used..?
+
+let result = []
+function fizzBuzz( counter = 1, num ) {
+    if ( counter === num + 1 ){ 
+        return result
+    } else if ( counter % 3 === 0 && counter % 5 === 0 ){
+        result.push("FizzBuzz")
+        return fizzBuzz( counter + 1, num )
+    } else if (counter % 3 === 0) {
+        result.push("Fizz")
+        return fizzBuzz( counter + 1, num )
+    } else if (counter % 5 === 0) {
+        result.push("Buzz")
+        return fizzBuzz( counter + 1, num )
     } else {
-        return final
+        result.push(counter)
+        return fizzBuzz( counter + 1, num )
     }
-    // return resultArr
 };
-console.log(fizzBuzz(6))
+fizzBuzz(1, 16)
+
+console.log(result)
+// fizzbuzz(16) results should equal: [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16]
+
+
+
+
+
+// TAIL CALL RECURSION TEST
+// let final = []
+// function tailCall(num) {
+//     if (num >= 1){
+//         final.push(num)
+//        return tailCall( num - 1)
+//     } else {
+//         return final
+//     }
+// };
+// console.log(tailCall(6))
