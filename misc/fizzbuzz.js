33c83d377b77b5f20a5086616b471647a4a61361
no0ro <nrolwes@gmail.com>
2021-07-13 23:38:31 -0400

    added prompt

---
+++ misc/fizzbuzz.js
@@ -0,0 +1,10 @@
+/* PROMPT 
+Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, 
+“Buzz” if an integer is divisible by 5, 
+and “FizzBuzz” if an integer is divisible by both 3 and 5
+*/
+
+
+
+
+
