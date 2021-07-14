8fcb5d3385288d7f8bf2af3066b58716ff912afc
no0ro <nrolwes@gmail.com>
2021-07-14 00:21:08 -0400

    added recursive fizzbuzz file and scaffold/prompt

---
+++ misc/fizzbuzz/recursive_fizzbuzz.js
@@ -0,0 +1,12 @@
+/* PROMPT
+Print integers 1 to N, but print “Fizz” if an integer is divisible by 3, 
+“Buzz” if an integer is divisible by 5, 
+and “FizzBuzz” if an integer is divisible by both 3 and 5
+*/
+
+function fizzBuzz() {
+
+};
+
+let x = fizzBuzz()
+console.log(x)
