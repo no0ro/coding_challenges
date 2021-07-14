16059c228e3b6fe248da4c76cbd6c9fa35588f18
no0ro <nrolwes@gmail.com>
2021-07-14 00:53:43 -0400

    test case for tail call recursion worked

--- misc/fizzbuzz/recursive_fizzbuzz.js
+++ misc/fizzbuzz/recursive_fizzbuzz.js
@@ -4,9 +4,23 @@ Print integers 1 to N, but print “Fizz” if an integer is divisible by 3,
 and “FizzBuzz” if an integer is divisible by both 3 and 5
 */
 
-function fizzBuzz() {
+// (i % 3 === 0 && i % 5 === 0)
 
+
+let final = []
+function fizzBuzz(num) {
+
+    // let resultArr = []
+    // let num = n
+    if (num >= 1){
+    //    resultArr.push(num)
+    //    console.log(num)
+    //    let updatedResult = resultArr
+        final.push(num)
+       return fizzBuzz( num - 1)
+    } else {
+        return final
+    }
+    // return resultArr
 };
-
-let x = fizzBuzz()
-console.log(x)
+console.log(fizzBuzz(6))
