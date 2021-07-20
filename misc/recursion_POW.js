273f8a505dc36867ece4a5203a07af138a0bd456
no0ro <nrolwes@gmail.com>
2021-07-19 23:12:49 -0400

    added comments and step by step break down

--- misc/recursion_POW.js
+++ misc/recursion_POW.js
@@ -11,10 +11,20 @@ Output: {Number} - expected
 */
 
 function  pow( base, exponent, newBase = base) { 
-	return exponent <= 1 ? newBase : pow(base, exponent - 1, base * newBase)
+    return exponent <= 1 ? newBase : pow(base, exponent - 1, base * newBase)
+    // use exponent arg as stop condition
+    // use newBase as counter/storage
 }
 
 console.log(pow(2, 3));
 console.log(pow(2, 4)); // -> 16
 console.log(pow(3, 5)); // -> 243
 
+
+/* -> Break Down of each recursive call 
+pow(2,4)
+    2, 4-1, 2*2 // 2,3,4
+    2, 3-1, 2*4 // 2,2,8
+    2, 2-1, 2*8 // 2,1,16
+    exponent = 1;
+*/
