40f8570addc6f8069ccc869dfeaee875776e6f78
no0ro <nrolwes@gmail.com>
2021-07-19 23:03:30 -0400

    added prompt

---
+++ misc/recursion_POW.js
@@ -0,0 +1,11 @@
+/* PROMPT
+
+Write a function that takes two inputs, a base and an exponent, 
+and returns the expected value of base ^ exponent. 
+For instance, if our base is 2 and our exponent is 3, then return 8 because 2^3 = 2*2*2 = 8.
+
+Input 1: {Number} base - base number raised to the exponent
+Input 2: {Number} exponent - exponent the base is raised to
+Output: {Number} - expected 
+
+*/
