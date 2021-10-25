124dae14acfb23da68a46257ee56b95572d279e6
no0ro <nrolwes@gmail.com>
2021-10-25 09:00:57 -0400

    skeleton

--- codewars/javascript/001.js
+++ codewars/javascript/001.js
@@ -4,3 +4,28 @@
 //   comprised of odd integers or entirely comprised of even integers except for
 //   a single integer N. Write a method that takes the array as an argument and
 //   returns this "outlier" N.
+
+function findOutlier(integers){
+    //check if we are searching for odds or evens
+    let evensCount = 0;
+    let oddsCount = 0;
+  
+    for (let i =0; i < integers.length; i++){
+    
+    }
+  
+    console.log(evensCount)
+    console.log(oddsCount)
+  
+    if (evensCount > oddsCount) { //all evens
+
+    }
+    if (evensCount < oddsCount) { //all oddss
+      
+    }
+    
+  }
+  
+  
+  
+  findOutlier([0, 1, 2]) //1
