8cb78f16d3cecb94db0e820d1bd8e6483cd4efc0
no0ro <nrolwes@gmail.com>
2021-11-01 00:00:37 -0400

    test code

--- misc/closure_unions.js
+++ misc/closure_unions.js
@@ -13,6 +13,13 @@ function union(nestedArrays) {
     }, [])
   };
 
+const arr1 = [5, 10, 15];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [100, 15, 10, 1, 5];
+console.log(union( [arr1, arr2, arr3] )); // should log: [5, 10, 15, 88, 1, 7, 100]
+
+
+
 
   /*
 // with comments & output
