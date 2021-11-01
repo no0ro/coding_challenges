cf48d26d97ff5167aae3c3a444242c126d131260
no0ro <nrolwes@gmail.com>
2021-10-31 23:59:13 -0400

    solution

--- misc/closure_unions.js
+++ misc/closure_unions.js
@@ -1,1 +1,14 @@
 // prompt
+
+
+
+function union(nestedArrays) {
+    let flattened = nestedArrays.flat(Infinity)
+  
+    return flattened.reduce(function (accum, currentVal) {
+      if (accum.indexOf(currentVal) === -1) {
+        accum.push(currentVal);
+      }
+      return accum
+    }, [])
+  };
