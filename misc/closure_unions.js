15f2425109455bc25a7813a05389385765fb36ba
no0ro <nrolwes@gmail.com>
2021-10-31 23:59:54 -0400

    commented

--- misc/closure_unions.js
+++ misc/closure_unions.js
@@ -12,3 +12,22 @@ function union(nestedArrays) {
       return accum
     }, [])
   };
+
+
+  /*
+// with comments & output
+function union(nestedArrays) {
+    let flattened = nestedArrays.flat(Infinity)
+    //accum = 5  currentVal = 10
+    return flattened.reduce(function (accum, currentVal) {
+      console.log(`accum ${accum}`)
+          console.log(`currentVal ${currentVal}`)
+      if (accum.indexOf(currentVal) === -1) {
+        accum.push(currentVal);
+      }
+      return accum
+    } ,[])
+  }
+  */
+  
+  
