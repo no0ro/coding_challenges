7ca0502c17e42365f7f9a630e3f418c8a0cfe4e4
no0ro <nrolwes@gmail.com>
2021-11-02 13:02:00 -0400

    added intersection with prompt

---
+++ misc/cb_intersection.js
@@ -0,0 +1,9 @@
+// prompt
+// Construct a fn `intersection` that compares input arrays and returns
+// a new array with elements found in all the inputs. BONUS - use reduce
+
+// start with this: 
+// const arr1 = [5, 10, 3, 15, 20];
+// const arr2 = [15, 88, 1, 3,  5, 7];
+// const arr3 = [1, 10, 15, 5, 20];
+// console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
