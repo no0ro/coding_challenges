9813205127ff3e83c2f87d4ea5960e9d043a51f9
no0ro <nrolwes@gmail.com>
2021-11-17 23:07:56 -0500

    added solution

--- misc/ec_passingArgs.js
+++ misc/ec_passingArgs.js
@@ -1,3 +1,16 @@
 // prompt: Examine the code given to you. Determine what arguments must be passed into isX1 and isX2 
 // respectively to obtain an output of true for both, and pass in the arguments accordingly.
 
+const x = 3;
+
+function isX1(num) {
+    const x = 5;
+    return num === x;
+}
+
+function isX2(num) {
+    return num === x;
+}
+
+const one = isX1(5);
+const two = isX2(3);
