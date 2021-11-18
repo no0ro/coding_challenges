86126bdc5ec25ff631d38207bfb73927ca83f930
no0ro <nrolwes@gmail.com>
2021-11-17 23:05:38 -0500

    added prompt

--- misc/ec_passingArgs.js
+++ misc/ec_passingArgs.js
@@ -1,1 +1,3 @@
-// prompt:
+// prompt: Examine the code given to you. Determine what arguments must be passed into isX1 and isX2 
+// respectively to obtain an output of true for both, and pass in the arguments accordingly.
+
